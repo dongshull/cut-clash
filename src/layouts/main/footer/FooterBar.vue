27ad750665848f832616a1ee8e9a6c552f5112fb
shihairu22 <84317517+shihairu22@users.noreply.github.com>
2024-07-28 09:00:15 +0800

    Update FooterBar.vue

--- src/layouts/main/footer/FooterBar.vue
+++ src/layouts/main/footer/FooterBar.vue
@@ -3,7 +3,7 @@
     <div class="footer-bottom py-3">
       <div class="container" style="text-align: center">
         <div class="mb-2 mb-md-0">
-          <span>好用的订阅转换页面</span>
+          <span>小石头的订阅转换</span>
         </div>
       </div>
     </div>
