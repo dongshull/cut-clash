9047ea39e87aa5e4a039e7550a2f7200bbe1aff7
shihairu22 <84317517+shihairu22@users.noreply.github.com>
2024-08-04 02:29:29 +0800

    Update SubTable.vue

--- src/views/home/SubTable.vue
+++ src/views/home/SubTable.vue
@@ -6,95 +6,75 @@
           <div class="card mb-4">
             <div class="card-body">
               <div class="row mb-3 g-3">
-                <div class="col-12 col-md-12">
+                <!-- 订阅链接 -->
+                <div class="col-12">
                   <label class="form-label" for="add-user-email">订阅链接</label>
                   <textarea class="form-control" v-model.trim="urls" :placeholder="placeholder" rows="3"></textarea>
                 </div>
-                <div class="col-5 col-md-6">
+                <!-- 客户端选择 -->
+                <div class="col-6">
                   <label class="form-label" for="client">客户端</label>
                   <select class="form-select" id="client" v-model="target" @change="selectTarget">
-                    <option v-for="option in targetOptions" :key="option" :value="option.value">
+                    <option v-for="option in targetOptions" :key="option.value" :value="option.value">
                       {{ option.text }}
                     </option>
                   </select>
                 </div>
-                <div class="col-7 col-md-6">
+                <!-- 后端服务选择 -->
+                <div class="col-6">
                   <label class="form-label" for="api">后端服务</label>
-                  <select class="form-select" id="api" @change="selectApi">
+                  <select class="form-select" id="api" v-model="api" @change="toggleManualApiUrl">
                     <option value="">请选择后端</option>
-                    <option v-for="option in apiUrl" :key="option" :value="option.value">
+                    <option v-for="option in apiUrl" :key="option.value" :value="option.value">
                       {{ option.text }}
                     </option>
                     <option value="manual">自定义后端 API 地址</option>
                   </select>
+                  <input v-if="isShowManualApiUrl" class="form-control mt-2" placeholder="自定义后端API地址" v-model="api" />
                 </div>
-                <div class="col-12 col-md-12" v-if="isShowManualApiUrl">
-                  <input class="form-control" placeholder="自定义后端API地址" v-model="api" />
-                </div>
-                <div class="col-8 col-md-10">
+                <!-- 远程配置与参数按钮 -->
+                <div class="col-8">
                   <label class="form-label" for="remote">远程配置</label>
-                  <select class="form-select" id="remote" @change="selectRemoteConfig">
+                  <select class="form-select" id="remote" v-model="remoteConfig" @change="toggleManualRemoteConfig">
                     <option value="">请选择配置</option>
-                    <option v-for="option in remoteConfigOptions" :key="option" :value="option.value">
+                    <option v-for="option in remoteConfigOptions" :key="option.value" :value="option.value">
                       {{ option.text }}
                     </option>
                     <option value="manual">自定义远程配置地址</option>
                   </select>
+                  <input v-if="isShowRemoteConfig" class="form-control mt-2" placeholder="自定义远程配置地址" v-model="remoteConfig" />
                 </div>
-                <div class="col-4 col-md-2 d-flex align-items-end">
-                  <button type="button" class="btn btn-warning w-100" @click="showMoreConfig">参数</button>
+                <div class="col-4 d-flex align-items-end">
+                  <button type="button" class="btn btn-warning w-100" @click="toggleMoreConfig">参数</button>
                 </div>
-                <div class="col-12 col-md-12" v-if="isShowRemoteConfig">
-                  <input class="form-control" placeholder="自定义远程配置地址" v-model="remoteConfig" />
-                </div>
-                <div class="col-12 col-md-12" v-if="isShowMoreConfig">
-                  <label class="form-label" for="add-user-email">可选参数</label>
+                <!-- 可选参数 -->
+                <div v-if="isShowMoreConfig" class="col-12">
+                  <label class="form-label" for="optional-params">可选参数</label>
                   <div class="row g-3">
-                    <div class="col-12 col-md-12">
-                      <input class="form-control" placeholder="Include: 可选" v-model="moreConfig.include" />
-                    </div>
-                    <div class="col-12 col-md-12">
-                      <input class="form-control" placeholder="Exclude: 可选" v-model="moreConfig.exclude" />
-                    </div>
-                    <div class="col-md check-div" :style="{ display: 'flex', flexWrap: 'wrap' }">
-                      <div class="form-check form-check-inline">
-                        <input class="form-check-input" type="checkbox" id="emoji" v-model="moreConfig.emoji" />
-                        <label class="form-check-label" for="emoji">Emoji</label>
-                      </div>
-                      <div class="form-check form-check-inline">
-                        <input class="form-check-input" type="checkbox" id="udp" v-model="moreConfig.udp" />
-                        <label class="form-check-label" for="udp">开启UDP</label>
-                      </div>
-                      <div class="form-check form-check-inline">
-                        <input class="form-check-input" type="checkbox" id="sort" v-model="moreConfig.sort" />
-                        <label class="form-check-label" for="sort">排序节点</label>
-                      </div>
-                      <div class="form-check form-check-inline">
-                        <input class="form-check-input" type="checkbox" id="scv" v-model="moreConfig.scv" />
-                        <label class="form-check-label" for="scv">关闭证书检查</label>
-                      </div>
-                      <div class="form-check form-check-inline">
-                        <input class="form-check-input" type="checkbox" id="nodelist" v-model="moreConfig.list" />
-                        <label class="form-check-label" for="nodelist">Node List</label>
-                      </div>
+                    <input v-model="moreConfig.include" class="form-control" placeholder="Include: 可选" />
+                    <input v-model="moreConfig.exclude" class="form-control" placeholder="Exclude: 可选" />
+                    <div class="form-check-inline" v-for="(label, key) in checkboxOptions" :key="key">
+                      <input class="form-check-input" type="checkbox" :id="key" v-model="moreConfig[key]" />
+                      <label class="form-check-label" :for="key">{{ label }}</label>
                     </div>
                   </div>
                 </div>
-                <div class="col-12 col-md-12">
+                <!-- 转换和短链生成 -->
+                <div class="col-12">
                   <div class="divider divider-dashed">
                     <div class="divider-text"><i class="ti ti-refresh" style="color: gray"></i></div>
                   </div>
                 </div>
-                <div class="col-12 col-md-10">
+                <div class="col-10">
                   <input class="form-control" placeholder="点击转换链接" v-model.trim="result.subUrl" />
                 </div>
-                <div class="col-12 col-md-2">
+                <div class="col-2">
                   <button type="button" class="btn btn-success w-100" @click="getSubUrl()">转换</button>
                 </div>
-                <div class="col-12 col-md-10">
+                <div class="col-10">
                   <input class="form-control" placeholder="点击获取短链" v-model.trim="result.shortUrl" />
                 </div>
-                <div class="col-12 col-md-2">
+                <div class="col-2">
                   <button type="button" class="btn btn-primary w-100" @click="getShortUrl()">短链</button>
                 </div>
               </div>
@@ -105,8 +85,8 @@
     </div>
   </div>
 
-  <!-- 弹窗结构 -->
-  <dialog id="errorDialog" ref="errorDialog">
+  <!-- 错误弹窗 -->
+  <dialog ref="errorDialog">
     <div>
       <p>{{ dialogMessage }}</p>
       <button @click="closeDialog">关闭</button>
@@ -122,20 +102,6 @@ import showNotification from '@/components/notification';
 
 export default {
   name: 'SubTable',
-  setup() {
-    const DEFAULT_MORECONFIG = {
-      include: '',
-      exclude: '',
-      emoji: true,
-      udp: true,
-      sort: false,
-      scv: false,
-      list: false,
-    };
-    return {
-      DEFAULT_MORECONFIG,
-    };
-  },
   data() {
     return {
       placeholder: '多订阅链接或节点请确保每行一条\n支持手动使用"|"分割多链接或节点',
@@ -159,79 +125,65 @@ export default {
       apiUrl: window.config.apiUrl,
       shortUrl: window.config.shortUrl,
       remoteConfigOptions: window.config.remoteConfigOptions,
-      moreConfig: this.DEFAULT_MORECONFIG,
-      isShowMoreConfig: false,
-      isShowManualApiUrl: false,
-      isShowRemoteConfig: false,
-      result: {
-        subUrl: '',
-        shortUrl: '',
+      moreConfig: {
+        include: '',
+        exclude: '',
+        emoji: true,
+        udp: true,
+        sort: false,
+        scv: false,
+        list: false,
       },
+      checkboxOptions: {
+        emoji: 'Emoji',
+        udp: '开启UDP',
+        sort: '排序节点',
+        scv: '关闭证书检查',
+        list: 'Node List',
+      },
+      result: { subUrl: '', shortUrl: '' },
       urls: [],
       api: window.config.apiUrl,
       target: 'clash',
       remoteConfig: '',
+      isShowMoreConfig: false,
+      isShowManualApiUrl: false,
+      isShowRemoteConfig: false,
       dialogMessage: '',
     };
   },
   methods: {
-    showMoreConfig() {
+    toggleMoreConfig() {
       this.isShowMoreConfig = !this.isShowMoreConfig;
     },
-    selectApi(event) {
-      if (event.target.value == 'manual') {
-        this.api = '';
-        this.isShowManualApiUrl = true;
-      } else {
-        this.isShowManualApiUrl = false;
-        this.api = event.target.value;
-      }
+    toggleManualApiUrl() {
+      this.isShowManualApiUrl = this.api === 'manual';
     },
-    selectRemoteConfig(event) {
-      if (event.target.value == 'manual') {
-        this.remoteConfig = '';
-        this.isShowRemoteConfig = true;
-      } else {
-        this.isShowRemoteConfig = false;
-        this.remoteConfig = event.target.value;
-      }
+    toggleManualRemoteConfig() {
+      this.isShowRemoteConfig = this.remoteConfig === 'manual';
     },
     async getSubUrl() {
+      if (!regexCheck(this.api)) return this.showErrorDialog('错误的后端API地址');
+      showLoading('生成中...');
       try {
-        showLoading('生成中...');
-        if (!regexCheck(this.api)) {
-          this.showErrorDialog('错误的后端API地址');
-          return;
-        }
-        const response = await getSubLink(this.api, {
+        this.result.subUrl = await getSubLink(this.api, {
           urls: this.urls,
           target: this.target,
           moreConfig: this.moreConfig,
           remoteConfig: this.remoteConfig,
         });
-        this.result.subUrl = response;
-        this.$forceUpdate();
       } catch (e) {
-        showNotification({
-          type: 'error',
-          title: '出错了',
-          message: e.toString(),
-        });
+        showNotification({ type: 'error', title: '出错了', message: e.toString() });
       } finally {
         hideLoading();
       }
     },
     async getShortUrl() {
+      showLoading('生成中...');
       try {
-        showLoading('生成中...');
-        const response = await request(`${this.shortUrl}/api`, 'post', {
-          url: this.result.subUrl,
-        });
-        if (response.code == 200) {
-          this.result.shortUrl = response.data;
-        } else {
-          this.showErrorDialog(response.message || '出错了');
-        }
+        const { code, data, message } = await request(`${this.shortUrl}/api`, 'post', { url: this.result.subUrl });
+        if (code === 200) this.result.shortUrl = data;
+        else this.showErrorDialog(message || '出错了');
       } catch (e) {
         this.showErrorDialog(e.toString());
       } finally {
@@ -252,7 +204,6 @@ export default {
 <style scoped>
 .custom-div {
   margin-bottom: 0;
-  padding-bottom: 0;
 }
 
 .d-flex.align-items-end {
@@ -260,12 +211,5 @@ export default {
   align-items: flex-end;
 }
 
-html, body {
-  height: 100%;
-  overflow: hidden;
-}
-
 .btn {
-  height: calc(100% - 10px);
-}
-</style>
+  height: calc(100
