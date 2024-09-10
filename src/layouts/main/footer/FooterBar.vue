17a3da2d41c8b54973b3b1e9439f332c5e7647b2
东叔 <75795765+dongshull@users.noreply.github.com>
2024-09-10 11:10:50 +0800

    Update 页脚

--- src/layouts/main/footer/FooterBar.vue
+++ src/layouts/main/footer/FooterBar.vue
@@ -4,8 +4,7 @@
       <div class="container" style="text-align: center">
         <div class="mb-2 mb-md-0">
            <a><img style='display:inline-block;vertical-align:middle' alt="本站支持IPv6访问" src="https://static.ipw.cn/icon/ipv6-s4.svg"><br></a>
-           <span>Design By Dong Shu | Support IPv6.<br></span> 
-           <span>Copyright © 2024 全影自动坊. All rights reserved.</span>
+           <span>Copyright © 全影自动坊 - DongShu. All rights reserved.</span>
         </div>
       </div>
     </div>
