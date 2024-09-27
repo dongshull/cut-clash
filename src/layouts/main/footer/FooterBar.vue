fd0638096066136454fb022e039fce131f68adb6
东叔 <75795765+dongshull@users.noreply.github.com>
2024-09-27 12:32:23 +0800

    Update FooterBar.vue

--- src/layouts/main/footer/FooterBar.vue
+++ src/layouts/main/footer/FooterBar.vue
@@ -4,7 +4,6 @@
       <div class="container" style="text-align: center">
         <div class="mb-2 mb-md-0">
            <a><img style='display:inline-block;vertical-align:middle' alt="本站支持IPv6访问" src="https://static.ipw.cn/icon/ipv6-s4.svg">
-             <img style='display:inline-block;vertical-align:middle' alt="本站支持SSL安全访问" src="https://static.ipw.cn/icon/ssl-s4.svg">
            </a>
           <br>
         </a>
