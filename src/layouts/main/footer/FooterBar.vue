a11eb269af9dacac09e1227b2d2fb02f36532aef
东叔 <75795765+dongshull@users.noreply.github.com>
2024-09-27 12:24:55 +0800

    Update sssl FooterBar.vue

--- src/layouts/main/footer/FooterBar.vue
+++ src/layouts/main/footer/FooterBar.vue
@@ -3,7 +3,8 @@
     <div class="footer-bottom py-3">
       <div class="container" style="text-align: center">
         <div class="mb-2 mb-md-0">
-           <a><img style='display:inline-block;vertical-align:middle' alt="本站支持IPv6访问" src="https://static.ipw.cn/icon/ipv6-s4.svg"><br></a>
+           <a><img style='display:inline-block;vertical-align:middle' alt="本站支持IPv6访问" src="https://static.ipw.cn/icon/ipv6-s4.svg">
+             <a><img style='display:inline-block;vertical-align:middle' alt="本站支持SSL安全访问" src="https://static.ipw.cn/icon/ssl-s4.svg"></a><br></a>
            <span>Copyright © 全影自动坊 - DongShu. All rights reserved.</span>
         </div>
       </div>
